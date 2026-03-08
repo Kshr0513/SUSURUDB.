6947bb0ae8733075d978d4047ff9c9a4b149d279
kshrhrhrhr <d34k03a221@icloud.com>
2026-03-08 15:42:23 +0900

    Initial Commit

---
+++ SUSURUDB./SUSURUDB_App.swift
@@ -0,0 +1,17 @@
+//
+//  SUSURUDB_App.swift
+//  SUSURUDB.
+//
+//  Created by kasaharadaiga on 2026/03/08.
+//
+
+import SwiftUI
+
+@main
+struct SUSURUDB_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
